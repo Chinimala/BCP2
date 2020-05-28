3a9c679744292a459a7004a689bab8c6454ea575
Chinimala <astrid.allemandou@free.fr>
2020-05-28 11:07:06 +0200

    Finished dex02

---
+++ ex02/logger.py
@@ -0,0 +1,65 @@
+import time
+from random import randint
+import functools
+import getpass
+
+
+def log(func):
+    """Print info about the decorated function"""
+    @functools.wraps(func)
+    def wrapper_log(*args, **kwargs):
+        start_time = time.perf_counter()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        time_unit = 's'
+        if run_time < 1:
+            run_time /= 1000
+            time_unit = 'ms'
+        func_name = ' '.join(func.__name__.split('_')).title()
+        f = open("machine.log", "a")
+        f.write("({})Running: {:20}[ exec-time = {:.3f} {:2} ]\n".
+                format(getpass.getuser(), func_name, run_time, time_unit))
+        f.close()
+        return value
+    return wrapper_log
+
+
+class CoffeeMachine():
+
+    water_level = 100
+
+    @log
+    def start_machine(self):
+        if self.water_level > 20:
+            return True
+        else:
+            print("Please add water!")
+            return False
+
+    @log
+    def boil_water(self):
+        return "boiling..."
+
+    @log
+    def make_coffee(self):
+        if self.start_machine():
+            for _ in range(20):
+                time.sleep(0.1)
+            self.water_level -= 1
+            print(self.boil_water())
+            print("Coffee is ready!")
+
+    @log
+    def add_water(self, water_level):
+        time.sleep(randint(1, 5))
+        self.water_level += water_level
+        print("Blub blub blub...")
+
+
+if __name__ == "__main__":
+    machine = CoffeeMachine()
+    for i in range(0, 5):
+        machine.make_coffee()
+    machine.make_coffee()
+    machine.add_water(70)

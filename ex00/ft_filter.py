b6351ea8d16d1aec753372f9f89afa3a9da5584c
Chinimala <astrid.allemandou@free.fr>
2020-05-28 11:10:48 +0200

    Normed ex00 and ex01

--- ex00/ft_filter.py
+++ ex00/ft_filter.py
@@ -1,17 +1,19 @@
 def ft_filter(function_to_apply, list_of_inputs):
-	res = []
-	if function_to_apply is None:
-		for elem in list_of_inputs:
-			if elem:
-				res.append(elem)
-	else:
-		for elem in list_of_inputs:
-			if function_to_apply(elem):
-				res.append(elem)
-	return res
+    res = []
+    if function_to_apply is None:
+        for elem in list_of_inputs:
+            if elem:
+                res.append(elem)
+    else:
+        for elem in list_of_inputs:
+            if function_to_apply(elem):
+                res.append(elem)
+    return res
+
 
 # list of alphabets
 alphabets = ['a', 'b', 'd', 'e', 'i', 'j', 'o', False, True]
+
 
 # function that filters vowels
 def filterVowels(alphabet):
@@ -20,6 +22,7 @@ def filterVowels(alphabet):
         return True
     else:
         return False
+
 
 filteredVowels = ft_filter(filterVowels, alphabets)
 print('The filtered vowels are:')

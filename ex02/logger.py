6bac3d830ef7e95296270b049c94bd2f57d5a6b4
Chinimala <astrid.allemandou@free.fr>
2020-05-28 21:11:53 +0200

    Finished ex03

--- ex02/logger.py
+++ ex02/logger.py
@@ -8,19 +8,18 @@ def log(func):
     """Print info about the decorated function"""
     @functools.wraps(func)
     def wrapper_log(*args, **kwargs):
-        start_time = time.perf_counter()    # 1
+        start_time = time.perf_counter()
         value = func(*args, **kwargs)
-        end_time = time.perf_counter()      # 2
-        run_time = end_time - start_time    # 3
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
         time_unit = 's'
         if run_time < 1:
             run_time /= 1000
             time_unit = 'ms'
         func_name = ' '.join(func.__name__.split('_')).title()
-        f = open("machine.log", "a")
-        f.write("({})Running: {:20}[ exec-time = {:.3f} {:2} ]\n".
-                format(getpass.getuser(), func_name, run_time, time_unit))
-        f.close()
+        with open("machine.log", "a") as f:
+        	f.write("({})Running: {:20}[ exec-time = {:.3f} {:2} ]\n".
+                	format(getpass.getuser(), func_name, run_time, time_unit))
         return value
     return wrapper_log
 
